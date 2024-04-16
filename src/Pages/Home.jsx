c2ee6a39327811feed706f02ae5c25e7a097c1c3
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-04-16 06:51:07 +0600

    tailwind, daisyui and font added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-playfair">This is home</h2>
+            <p>alu</p>
+            <p>alu</p>
+            <p>alu</p>
+        </div>
+    );
+};
+
+export default Home;
